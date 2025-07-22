f62d43092d596359709f8db451bf034956c71dbd
Manjusha508 <manjugalanki@gmail.com>
2025-07-22 22:43:27 +0530

    Update settings.py

--- omnify/settings.py
+++ omnify/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fitness',
-        'USER':'postgres',
+        'USER':'########',
         'PASSWORD':'****',
         'HOST':'localhost',
         'PORT': '5432',
